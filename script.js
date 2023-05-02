6f9c56c6db70d8f6922484a68d0b30fcfc7e5b3e
ADMIN <ale.ga.ca90@gmail.com>
2023-05-02 15:12:11 -0600

    function already

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-const cards= document.getElementsByClassName("card")
+const cards= document.querySelectorAll(".card")
 
+// console.log(cards[3])
+ cards.forEach(card=>{
+    // console.log(card) revisar que el for each este funcionando
+card.addEventListener("click", ()=>{
+    // console.log("si funciona cada click") revisar si esta funcionando mis clicks
+    // en cada click activar la clase active
+    removeClassActive();
+    card.classList.add("active")
+}) 
+}
 
- cards.addEventListener('click', () => {
-        alert("it woraaaaaaaks")
-    })
+ )
+
+ function removeClassActive (){
+    cards.forEach(card =>{
+        card.classList.remove("active")
+    }
+
+    )
+ }
+//  cards.addEventListener('click', () => {
+//         alert("it woraaaaaaaks")
+//     })
